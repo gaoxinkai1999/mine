1c1718e6b4b5d3fcfb44f35730c0c5c2930eaa2a
g <1102336460@qq.com>
2025-03-07 23:12:16 +0800

    ﻿v1.2.2: 优化打印功能

---
+++ criptsgit-hooksprepare-commit-msg.ps1
@@ -0,0 +1,5 @@
+[33mcommit a5d01859772f3673faad0935757408b6bbcffde4[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m)[m
+Author: g <1102336460@qq.com>
+Date:   Fri Mar 7 23:24:16 2025 +0800
+
+    删除钩子后的提交
